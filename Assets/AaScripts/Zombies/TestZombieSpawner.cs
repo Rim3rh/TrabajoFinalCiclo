a4eefac07b781f7638907d346017706a853b8da2
Mario Ribe Herrero <124402943+Rim3rh@users.noreply.github.com>
2024-04-04 13:57:09 +0300

    mapa nuevo
    Implementar assets mapa nuevo parcialmente + prueba con sicronizacion armas y prueba con spawn zombies

--- Assets/AaScripts/Zombies/TestZombieSpawner.cs
+++ Assets/AaScripts/Zombies/TestZombieSpawner.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 
 public class TestZombieSpawner : NetworkBehaviour
 {
-    [SerializeField] Transform spawnPos;
+    [SerializeField] Transform[] spawnPositions;
     [SerializeField] ZombiePoolManager poolManager;
 
     private void Start()
     {
         if (!IsServer) return;
-        Invoke(nameof(SpawnZombie), 10f);
     }
 
 
-    private void SpawnZombie()
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            SpawnZombie(spawnPositions[Random.Range(0, spawnPositions.Length)]);
+        }
+    }
+    private void SpawnZombie(Transform spanwpos)
     {
         GameObject zombie = poolManager.GetZombie();
         zombie.SetActive(true);
-        zombie.transform.position = spawnPos.position;
+        zombie.transform.position = spanwpos.position;
         zombie.GetComponent<ZombiesHealthController>().zombieHealth = 5;
         zombie.GetComponent<Animator>().SetTrigger("Rise");
         GetComponent<NetworkObject>().Spawn();
